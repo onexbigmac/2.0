e8a6acef22b0f4f7d2ef83368b6a02ef9a51dbed
onexbigmac <103407631+onexbigmac@users.noreply.github.com>
2025-06-18 13:45:53 +0200

    new

--- js/layout-text-right.js
+++ js/layout-text-right.js
@@ -19,7 +19,12 @@ class LayoutTextRight extends HTMLElement {
       
       this.innerHTML = ``
       this.innerHTML = `
-      <div class=" macComp ${colour}">
+      <div class=" macComp ${colour}  ">
+        <div class="parallax" data-overflow="true" data-orientation="up"  data-scale="2">
+            <div class="position-absolute w-100"><img src="./../img/recticle.png" class="position-relative rotating360 opacity-25" style="right: 0%;
+    left: 91%;
+    width: 320px;"></div>
+        </div>
         <div class="row align-items-center section-h">
           <div class="col-9 col-sm-7  ms-auto text-end mb-3 ">
             <div class="parallax" data-overflow="true" data-orientation="right"><slot name="text">${text}</slot></div>
