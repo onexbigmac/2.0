bfddff143244ea09f5a55607b1dfe9402ee94846
onexbigmac <103407631+onexbigmac@users.noreply.github.com>
2025-07-16 12:07:26 +0200

    updates

---
+++ layout-text-right.js
@@ -0,0 +1,54 @@
+import { parseSharedSlotsDynamic } from './slotParser.js';
+
+class LayoutTextRight extends HTMLElement {
+  connectedCallback() {
+    const $this = $(this);
+    const { 
+          colour, 
+          title, 
+          text, 
+          bullet1, 
+          bullet1content,
+          bullet2, 
+          bullet2content,
+          bullet3, 
+          bullet3content,
+          bullet4, 
+          bullet4content
+    } = parseSharedSlotsDynamic($this);
+      
+      this.innerHTML = ``
+      this.innerHTML = `
+      <div class=" macComp ${colour}  ">
+        <div class="parallax" data-overflow="true" data-orientation="up"  data-scale="2">
+            <div class="position-absolute w-100">
+                <img src="./../img/recticle.png" class="position-relative rotating360 " 
+                style="
+                        opacity:2%;
+
+                        width: 320px;">
+
+            </div>
+            <div class="position-absolute w-100" style="
+
+                        right: 0%;
+                        left: 91%; 
+                    ">
+                <img src="./../img/recticle.png" class="position-relative rotating360 " 
+                style="
+                        opacity:2%;
+                        animation-duration: 14s;
+                        width: 520px;">
+
+            </div>
+        </div>
+        <div class="row align-items-center section-h">
+          <div class="col-12 col-sm-9  ms-auto text-end mb-3 ">
+            <div class="parallax" data-overflow="true" data-orientation="right"><slot name="text">${text}</slot></div>
+          </div>
+        </div>
+      </div>   
+    `;
+  }
+}
+customElements.define('layout-text-right', LayoutTextRight);
---
+++ layout-three.js
@@ -0,0 +1,53 @@
+import { parseSharedSlotsDynamic } from './slotParser.js';
+
+class LayoutThree extends HTMLElement {
+  connectedCallback() {
+    const $this = $(this);
+    const { 
+          colour, 
+          title, 
+          text, 
+          bullet1, 
+          bullet1content,
+          bullet2, 
+          bullet2content,
+          bullet3, 
+          bullet3content,
+          bullet4, 
+          bullet4content
+    } = parseSharedSlotsDynamic($this);
+      
+    this.innerHTML = `
+<div class=" macComp ${colour}">
+    <div class="row justify-content-between h6">
+          <div class="col-12 col-sm-6 mb-3 col-sm-auto">
+            <div class="animate__animated animate__delay-1s animate-on-scroll" data-anim="animate__fadeInUp"> 
+                <div class="subHeading mb-2">${bullet1content} </div>
+                <div> ${bullet1}</div>
+            </div>
+
+          </div>
+          <div class="col-12 col-sm-6 mb-3 col-sm-auto">
+            <div class="animate__animated animate__delay-2s animate-on-scroll" data-anim="animate__fadeInUp"> 
+                <div class="subHeading mb-2">${bullet2content}</div>
+                <div> ${bullet2}</div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 mb-3 col-sm-auto">
+            <div class="animate__animated animate__delay-3s animate-on-scroll" data-anim="animate__fadeInUp"> 
+            <div class="subHeading mb-2">${bullet3content} </div>
+            <div> ${bullet3}</div>
+</div>
+          </div>
+          <div class="col-6 mb-3 col-sm-auto">
+            <div class="animate__animated animate__delay-4s animate-on-scroll" data-anim="animate__fadeInUp"> 
+                <div class="subHeading mb-2">${bullet4content} </div>
+                <div> ${bullet4}</div>
+            </div>
+          </div>
+    </div>
+</div>
+    `;
+  }
+}
+customElements.define('layout-three', LayoutThree);
